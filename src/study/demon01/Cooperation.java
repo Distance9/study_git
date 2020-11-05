f50f7934a837b80e1391d116dc7fb0603f513874
Distance9 <895399643@qq.com>
2020-11-05 19:35:29 +0800

    mike第一次提交

---
+++ src/study/demon01/Cooperation.java
@@ -0,0 +1,24 @@
+package study.demon01;
+
+/**
+ *
+ * 协作类
+ *
+ * @Description
+ * @Author 魔法はまだ解けない
+ * @Date 2020/11/5
+ */
+public class Cooperation {
+
+    public void writeByMike(String name){
+        System.out.println("你好，我是麦克");
+    }
+
+
+    public void writeByTom(String name){
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
